d511bd48a588169b87f16fdfd6634ca8eda46fa5
Бабин Юрий Юрьевич <babin@pcr.int>
2019-07-22 19:14:34 +0300

    putting all the things into one file

--- pyrama.py
+++ pyrama.py
@@ -1,22 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  9 12:55:16 2019
 
-from __future__ import division, print_function
-
-import math
+@author: babin
+"""
+import matplotlib.colors as mplcolors
 import os
-import sys
-
+import math
 import matplotlib.pyplot as plt
 import numpy as np
 from Bio import PDB
 from matplotlib import colors
 
-from config import RAMA_PREFERENCES
 
-RAMA_PREF_VALUES = None
+
+RAMA_PREFERENCES = {
+    "General": {
+        "file": os.path.join('data', 'pref_general.data'),
+        "cmap": mplcolors.ListedColormap(['#FFFFFF', '#B3E8FF', '#7FD9FF']),
+        "bounds": [0, 0.0005, 0.02, 1],
+    },
+    "GLY": {
+        "file": os.path.join('data', 'pref_glycine.data'),
+        "cmap": mplcolors.ListedColormap(['#FFFFFF', '#FFE8C5', '#FFCC7F']),
+        "bounds": [0, 0.002, 0.02, 1],
+    },
+    "PRO": {
+        "file": os.path.join('data', 'pref_proline.data'),
+        "cmap": mplcolors.ListedColormap(['#FFFFFF', '#D0FFC5', '#7FFF8C']),
+        "bounds": [0, 0.002, 0.02, 1],
+    },
+    "PRE-PRO": {
+        "file": os.path.join('data', 'pref_preproline.data'),
+        "cmap": mplcolors.ListedColormap(['#FFFFFF', '#B3E8FF', '#7FD9FF']),
+        "bounds": [0, 0.002, 0.02, 1],
+    }
+}
 
 
 def _cache_RAMA_PREF_VALUES():
-    f_path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    f_path = os.path.realpath(os.path.join(os.getcwd()))
     RAMA_PREF_VALUES = {}
     for key, val in RAMA_PREFERENCES.items():
         RAMA_PREF_VALUES[key] = np.full((360, 360), 0, dtype=np.float64)
@@ -41,9 +64,8 @@ def calc_ramachandran(file_name_list):
     :param file_name_list: List of PDB files to plot
     :return: Nothing
     """
-    if RAMA_PREF_VALUES is None:
-        global RAMA_PREF_VALUES
-        RAMA_PREF_VALUES = _cache_RAMA_PREF_VALUES()
+
+    RAMA_PREF_VALUES = _cache_RAMA_PREF_VALUES()
 
     # Read in the expected torsion angles
     normals = {}
@@ -64,7 +86,7 @@ def calc_ramachandran(file_name_list):
                     phi_psi = poly.get_phi_psi_list()
                     for res_index, residue in enumerate(poly):
                         res_name = "{}".format(residue.resname)
-                        res_num = residue.id[1]
+                        #res_num = residue.id[1] # not used 
                         phi, psi = phi_psi[res_index]
                         if phi and psi:
                             if str(poly[res_index + 1].resname) == "PRO":
@@ -87,12 +109,15 @@ def calc_ramachandran(file_name_list):
     
 def plot_ramachandran(normals, outliers):
 
+    RAMA_PREF_VALUES = None
     if RAMA_PREF_VALUES is None:
-        global RAMA_PREF_VALUES
+        
+        #contant RAMA_PREF_VALUES
         RAMA_PREF_VALUES = _cache_RAMA_PREF_VALUES()
         
 
     for idx, (key, val) in enumerate(sorted(RAMA_PREFERENCES.items(), key=lambda x: x[0].lower())):
+        plt.figure(figsize=(10,10))
         plt.subplot(2, 2, idx + 1)
         plt.title(key)
         plt.imshow(RAMA_PREF_VALUES[key], cmap=RAMA_PREFERENCES[key]["cmap"],
@@ -112,3 +137,7 @@ def plot_ramachandran(normals, outliers):
     plt.tight_layout()
     # plt.savefig("asd.png", dpi=300)
     plt.show()
+    
+    
+    
+      
